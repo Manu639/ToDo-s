dbba623c76bc2d5f15b00fddd2fc9c26b55be707
Manuel Castillo <m.castillo@gestionescastillo.com>
2021-07-24 14:08:24 +0200

    Finish left card layout: user and filters

---
+++ js/model.js
@@ -0,0 +1,22 @@
+const tasksList = new Array({
+    idTask: 0,
+    company: 'Prueba S.L',
+    taxType: 'Iva',
+    dueDate: new Date(2021, 5, 20),
+    relevance: 'Baja',
+    completed: false
+}, {
+    idTask: 0,
+    company: 'Prueba 2 S.L',
+    taxType: 'IS',
+    dueDate: new Date(2021, 6, 26),
+    relevance: 'Alta',
+    completed: false
+}, {
+    idTask: 0,
+    company: 'Prueba S.L',
+    taxType: 'Iva',
+    dueDate: new Date(2021, 5, 20),
+    relevance: 'Media',
+    completed: false
+})
